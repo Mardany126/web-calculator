41005dfc93315c020e8a6f7e6d5fc8894bbb6f54
Max Mardany <Max@Maxs-MacBook-Air.local>
2021-03-12 00:15:04 -0500

    answer branch reset

---
+++ script.js
@@ -0,0 +1,6 @@
+function sum() {
+    var num1 = parseInt(document.getElementById("num1").value)
+    var num2 = parseInt(document.getElementById("num2").value)   
+    
+    document.getElementById("answer").value = num1 + num2
+    }
