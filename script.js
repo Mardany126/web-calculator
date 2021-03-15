950f8d8cb43e9d87472e24a913b12032eaeb7e1f
Mardany126 <79369900+Mardany126@users.noreply.github.com>
2021-03-15 18:58:07 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function sum() {
-    var num1 = parseInt(document.getElementById("num1").value)
-    var num2 = parseInt(document.getElementById("num2").value)   
-    
-    document.getElementById("answer").value = num1 + num2
-    }
